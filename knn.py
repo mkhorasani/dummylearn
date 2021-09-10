1b45e07f6baa7b12916328e0593e4c685c1b293b
Mohammad Khorasani <khorasani.mohammad@gmail.com>
2021-09-10 15:56:54 +0300

    Added hyperparameters download, fixed update time

--- knn.py
+++ knn.py
@@ -15,25 +15,7 @@ from streamlit.server.server import Server
 from sqlalchemy import Table, Column, String, MetaData
 from datetime import datetime
 import os
-
-def insert_row(session_id,engine):
-    if engine.execute("SELECT session_id FROM session_state WHERE session_id = '%s'" % (session_id)).fetchone() is None:
-            engine.execute("""INSERT INTO session_state (session_id) VALUES ('%s')""" % (session_id))
-
-def update_row(column,new_value,session_id,engine):
-    engine.execute("UPDATE session_state SET %s = '%s' WHERE session_id = '%s'" % (column,new_value,session_id))
-
-def get_session():
-    session_id = get_report_ctx().session_id
-    session_info = Server.get_current()._get_session_info(session_id)
-
-    if session_info is None:
-        raise RuntimeError("Couldn't get your Streamlit Session object.")
-
-    session_id = session_id.replace('-','_')
-    session_id = '_id_' + session_id
-
-    return session_info.session, session_id
+from Utils import *
 
 def confusion_matrix_plot(data,labels):
     z = data.tolist()[::-1]
@@ -51,31 +33,6 @@ def roc_plot(data):
     fig.update_layout(font_family="IBM Plex Sans")
 
     st.write(fig)
-
-def download(df,filename): # Downloading DataFrame
-    csv = df.to_csv(index=False)
-    b64 = base64.b64encode(csv.encode()).decode()
-    href = (f'<a href="data:file/csv;base64,{b64}" download="%s.csv">Download csv file</a>' % (filename))
-    return href
-
-def file_upload(name):
-    uploaded_file = st.sidebar.file_uploader('%s' % (name),key='%s' % (name),accept_multiple_files=False)
-    content = False
-    if uploaded_file is not None:
-        try:
-            uploaded_df = pd.read_csv(uploaded_file)
-            content = True
-            return content, uploaded_df
-        except:
-            try:
-                uploaded_df = pd.read_excel(uploaded_file)
-                content = True
-                return content, uploaded_df
-            except:
-                st.error('Please ensure file is .csv or .xlsx format and/or reupload file')
-                return content, None
-    else:
-        return content, None
 
 def knn_main(engine):
 
@@ -124,7 +81,6 @@ def knn_main(engine):
 
             st.markdown('For further information please refer to ths [link](https://scikit-learn.org/stable/modules/generated/sklearn.neighbors.KNeighborsClassifier.html)')
 
-
         try:
             X = df[feature_cols]
             y = df[label_col]
@@ -154,6 +110,13 @@ def knn_main(engine):
                 st.info('Area Under Curve: **%s**' % (round(auc,3)))
             except:
                 pass
+
+            #Download hyperparameters
+            hyperparameters = {'n_neighbors':[number_neighbors], 'weights':[weights], 'algorithm':[algorithm],
+            'leaf_size':[leaf_size],'p':[p], 'metric':[metric]}
+
+            st.subheader('Download hyperparameters')
+            st.markdown(download(pd.DataFrame(hyperparameters),'DummyLearn.com - K-Nearest Neighbors Classifier - Hyperparameters'), unsafe_allow_html=True)
 
             st.sidebar.subheader('Test Dataset')
             status_test, df_test = file_upload('Please upload a test dataset')
